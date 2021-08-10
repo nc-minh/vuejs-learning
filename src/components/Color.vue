f4b7bc2cefb9426f83b82a358fed68fca5659c49
nguyencongminh-dev <nguyencongminh.official@gmail.com>
2021-08-10 18:00:16 +0700

    add: first time

---
+++ src/components/Color.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+      <input type="text" v-model="color">
+      <div class="squares" :class="color"></div>
+      <div class="squares" :style="customStyle"></div>
+      <input type="text" placeholder="chiều dài" v-model="width">
+      <input type="text" placeholder="chiều rộng" v-model="height">
+      <div class="squares"></div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            color: 'red',
+            check: false,
+            width: 200,
+            height: 200,
+        }
+    },
+
+    computed:{
+        customStyle: function(){
+            return{
+                backgroundColor: this.color,
+                width: this.width + 'px',
+                height: this.height + 'px'
+            }
+        }
+    }
+}
+</script>
+
+<style>
+.squares{
+    width: 100px;
+    height: 100px;
+    background-color: hotpink;
+    border: solid 5px #ccc;
+}
+
+.red{
+    background-color: red;
+}
+
+.blue{
+    background-color: blue;
+}
+
+.green{
+    background-color: green;
+}
+
+.default{
+    background-color: indigo;
+}
+</style>
---
+++ src/components/TagSelector.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="list">
+      <div 
+      class="item_wrap" 
+      v-for="(language, index) in languages" 
+      :key="index" 
+      @click="language.selected=!language.selected"
+      :class="{selected: language.selected}">
+          <span class="item">{{language.name}}</span>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            languages:[
+                { name: 'PHP', selected: false },
+                { name: 'Java', selected: true },
+                { name: 'JavaScipt', selected: true },
+                { name: 'Python', selected: false },
+                { name: 'Dark', selected: false },
+                { name: 'C++', selected: true },
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+    .list{
+        display: flex;
+        margin: 30px 0;
+        flex-wrap: wrap;
+    }
+
+    .item_wrap{
+        padding: 20px 30px;
+        margin: 10px 10px;
+        border-radius: 9999px;
+        box-shadow: 3px 5px 10px #888888;
+        cursor: pointer;
+        border: 3px solid tomato;
+    }
+
+    .selected{
+        background-color: teal;
+    }
+</style>
