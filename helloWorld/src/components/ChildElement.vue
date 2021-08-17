de6e31ff30303bead9ae63609ed8ab099cf5d4bb
= <nguyencongminh.official@gmail.com>
2021-08-17 12:42:46 +0700

    okk

---
+++ helloWorld/src/components/ChildElement.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <div class="child">
+            <button @click="yeucaubatdenxanh">bật đèn nhà xanh</button>
+            <hr>
+            <h1>{{ thuoctinh_dotat }}</h1>
+            <hr>
+            <h1>{{ thuoctinh_xanhtat }}</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    },
+    props:{
+        thuoctinh_dotat:{
+            type: String
+        },
+        thuoctinh_xanhtat:{
+            type: String
+        }
+    },
+    methods:{
+        yeucaubatdenxanh(){
+            console.log('trước khi kích hoạt')
+            this.$emit('hamxulixukien_batdenxanh')
+            console.log('sau khi kích hoạt')
+        }
+    }
+}
+</script>
+
+<style scoped>
+.child{
+    width: 300px;
+    height: 300px;
+    background-color: rgb(255, 8, 0);
+}
+</style>
